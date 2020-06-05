bf982680dd8b76d318392319a18a84c926e39df4
Wesle Leal <wesle.profissional@gmail.com>
2020-06-05 13:25:28 -0300

    App

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes'
+
+// JSX: sitaxe de XML dentro do javascript
+function App() {
+  return (
+       <Routes />
+  );
+}
+
+export default App;
